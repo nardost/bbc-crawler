4ce29bc4a5933cc261200576b51b648efdcdfe9d
nardos <32473919+nardost@users.noreply.github.com>
2020-03-13 12:10:13 -0500

    Programs list finder and episodes finer refactored to separate methods.

--- princeton-web-crawler/src/main/java/edu/depaul/ntessema/csc575/WebCrawler.java
+++ princeton-web-crawler/src/main/java/edu/depaul/ntessema/csc575/WebCrawler.java
@@ -15,153 +15,123 @@ public class WebCrawler {
      * The number of available BBC programs is to great to deal with
      * in this course project. So, I had to limit the programs to only
      * a subset of the entire program list.
+     *
+     * The base URLs were themselves obtained by crawling the a to z program list.
      */
     private static String[][] selectedPrograms = new String [][] {
-        { "The-Why-Factor", "https://www.bbc.co.uk/programmes/p00xtky9/episodes/player" },
-        { "The-Forum", "https://www.bbc.co.uk/programmes/p004kln9/episodes/player" },
-        { "Thinking-Allowed", "https://www.bbc.co.uk/programmes/b006qy05/episodes/player" },
-        { "In-Our-Time", "https://www.bbc.co.uk/programmes/b006qykl/episodes/player" },
-        { "Start-the-Week", "https://www.bbc.co.uk/programmes/b006r9xr/episodes/player" },
-        { "The-Fifth-Floor", "https://www.bbc.co.uk/programmes/p00mt9kd/episodes/player" },
-        { "Crossing-Continents", "https://www.bbc.co.uk/programmes/b006qt55/episodes/player" },
-        { "The-History-Hour", "https://www.bbc.co.uk/programmes/p016tmg1/episodes/player" },
-        { "Free-Thinking", "https://www.bbc.co.uk/programmes/b0144txn/episodes/player" },
-        { "The Documentary", "https://www.bbc.co.uk/programmes/p00fvhsf/episodes/player" },
-        { "Last-Word", "https://www.bbc.co.uk/programmes/b006qpmv/episodes/player" },
-        { "The-Essay", "https://www.bbc.co.uk/programmes/b006x3hl/episodes/player" },
-        { "Arts-and-Ideas", "https://www.bbc.co.uk/programmes/b0144txn/episodes/player" },
-        { "Heart-and-Soul", "https://www.bbc.co.uk/programmes/p002vsn4/episodes/player" },
-        { "Great-Lives", "https://www.bbc.co.uk/programmes/b006qxsb/episodes/player" },
-        { "Night-Waves", "https://www.bbc.co.uk/programmes/b006tp43/episodes/player" },
-        { "Meridian", "https://www.bbc.co.uk/programmes/p03m0hz8/episodes/guide" },
-        { "Discovery", "https://www.bbc.co.uk/programmes/p002w557/episodes/player" },
-        { "A-History-of-Ideas", "https://www.bbc.co.uk/programmes/b04bwydw/episodes/player" },
-        { "Outlook", "https://www.bbc.co.uk/programmes/p002vsxt/episodes/player" }
+        { "The-Why-Factor", "https://www.bbc.co.uk/programmes/p00xtky9/episodes/player" }
     };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        List<String> listOfPrograms = new ArrayList<>();
+        List<String> listOfPrograms = getSelectedProgramsList();
+        List<String> listOfEpisodes = getEpisodes(listOfPrograms);
+    }
 
-        for(String [] program : selectedPrograms) {
-            /*
-             * Limit the list even more...
-             */
-            if(program[0].equals("A-History-of-Ideas")) {
-                listOfPrograms.add(program[1]);
-            }
-        }
-
+    private static List<String> getEpisodes(List<String> listOfPrograms) throws IOException {
         /*
          * List that holds all episodes of all programs.
          */
         List<String> listOfEpisodes = new ArrayList<>();
-        int index = 0;
-        try {
 
-            final String cssClass = "div.programme__body h2.programme__titles a";
+        final String cssClass = "div.programme__body h2.programme__titles a";
 
-            int totalNumberOfEpisodesFound = 0;
+        int totalNumberOfEpisodesFound = 0;
+        /*
+         * Iterate over the list of programs found.
+         * Each program page could have multiple pages.
+         */
+        for(String programRootUrl : listOfPrograms) {
+
+            System.out.println(String.format("%31s", "TRYING TO CONNECT TO: ") + programRootUrl);
             /*
-             * Iterate over the list of programs found.
-             * Each program page could have multiple pages.
+             * Connect to the page at p.
              */
-            for(; index < listOfPrograms.size(); index++) {
+            Connection connection = Jsoup.connect(programRootUrl);
+            /*
+             * Disable exception throwing. Many things could go
+             * wrong while crawling, but we don't want that to
+             * stop execution of the crawler.
+             */
+            connection.ignoreHttpErrors(true);
+            System.out.println(String.format("%31s", "CONNECTED TO: ") + programRootUrl);
+            if(connection != null) {
                 /*
-                 * The link to the current program
+                 * Proceed only if http status is OK
                  */
-                String programRootUrl = listOfPrograms.get(index);
-                System.out.println("TRYING TO CONNECT TO " + programRootUrl);
-                /*
-                 * Connect to the page at p.
-                 */
-                Connection connection = Jsoup.connect(programRootUrl);
-                /*
-                 * Disable exception throwing. Many things could go
-                 * wrong while crawling, but we don't want that to
-                 * stop execution of the crawler.
-                 */
-                connection.ignoreHttpErrors(true);
-                System.out.println("        CONNECTED TO " + programRootUrl);
-                if(connection != null) {
+                int statusCode = connection.execute().statusCode();
+                if (statusCode == 200) {
                     /*
-                     * Proceed only if http status is OK
+                     * Get the DOM of the page at p
                      */
-                    int statusCode = connection.execute().statusCode();
-                    if (statusCode == 200) {
+                    Document d = Jsoup.connect(programRootUrl).get();
+                    if (d != null) {
                         /*
-                         * Get the DOM of the page at p
+                         * Find how many pages there are for this program
                          */
-                        Document d = Jsoup.connect(programRootUrl).get();
-                        if (d != null) {
+                        int totalNumberOfPages = 0;
+                        /*
+                         * Extract the pagination elements (<li>)
+                         */
+                        List<Element> paginationElements = d.select("div.programmes-page ol.pagination li.pagination__page");
+                        /*
+                         * If there are other pages than the current one
+                         * there will be pagination and, therefore, pagination elements.
+                         */
+                        if(paginationElements.size() > 0) {
                             /*
-                             * Find how many pages there are for this program
+                             * Get the last pagination element
                              */
-                            int totalNumberOfPages = 0;
+                            Element lastElement = paginationElements.get(paginationElements.size() - 1).getAllElements().last();
                             /*
-                             * Extract the pagination elements (<li>)
+                             * The total number of pages is the number in the last
+                             * pagination element.
                              */
-                            List<Element> paginationElements = d.select("div.programmes-page ol.pagination li.pagination__page");
+                            totalNumberOfPages = Integer.parseInt(lastElement.getElementsByTag("a").html());
+                            System.out.print(String.format("%31s", "PAGES: ") + totalNumberOfPages);
+                        }
+                        /*
+                         * Ready to visit every page...
+                         */
+                        int currentPage = 1;
+                        /*
+                         * The first page is at the programRootUrl
+                         * (https://www.bbc.co.uk/programmes/{programId}/episodes/player)
+                         */
+                        do {
+                            System.out.println(String.format("%31s", "NOW IN PAGE: ") + currentPage);
                             /*
-                             * If there are other pages than the current one
-                             * there will be pagination and, therefore, pagination elements.
+                             * all epiosodes in currentPage only
                              */
-                            if(paginationElements.size() > 0) {
+                            List<Element> episodes = d.select(cssClass);
+                            if (episodes != null) {
                                 /*
-                                 * Get the last pagination element
+                                 * Iterate over the episodes in the currentPage
                                  */
-                                Element lastElement = paginationElements.get(paginationElements.size() - 1).getAllElements().last();
-                                /*
-                                 * The total number of pages is the number in the last
-                                 * pagination element.
-                                 */
-                                System.out.print("Last Page: ");
-                                totalNumberOfPages = Integer.parseInt(lastElement.getElementsByTag("a").html());
+                                episodes.forEach(e -> {
+                                    /*
+                                     * Add the link to the discovered episodes to the
+                                     * list that contains all episodes from all programs.
+                                     */
+                                    listOfEpisodes.add(e.attr("href"));
+                                });
+                                System.out.println(String.format("%31s", "NUMBER OF EPISODES FOUND:") + episodes.size());
+                                totalNumberOfEpisodesFound += episodes.size();
                             }
                             /*
-                             * Ready to visit every page...
+                             * The next page is programRootUrl?page={pageCount}
+                             * where programRootUrl = https://www.bbc.co.uk/programmes/{programId}/episodes/player
                              */
-                            int currentPage = 1;
-                            /*
-                             * The first page is at the programRootUrl
-                             * (https://www.bbc.co.uk/programmes/{programId}/episodes/player)
-                             */
-                            do {
-                                System.out.println("IN PAGE " + currentPage);
-                                /*
-                                 * all epiosodes in currentPage only
-                                 */
-                                List<Element> episodes = d.select(cssClass);
-                                if (episodes != null) {
-                                    /*
-                                     * Iterate over the episodes in the currentPage
-                                     */
-                                    episodes.forEach(e -> {
-                                        /*
-                                         * Add the link to the discovered episodes to the
-                                         * list that contains all episodes from all programs.
-                                         */
-                                        listOfEpisodes.add(e.attr("href"));
-                                    });
-                                    System.out.println("NUMBER OF EPISODES FOUND: " + episodes.size());
-                                    totalNumberOfEpisodesFound += episodes.size();
-                                }
-                                /*
-                                 * The next page is programRootUrl?page={pageCount}
-                                 * where probramRootUrl = https://www.bbc.co.uk/programmes/{programId}/episodes/player
-                                 */
-                                d = Jsoup.connect(programRootUrl + "?page=" + currentPage++).get();
-                            } while(currentPage <= totalNumberOfPages);
-                        }
+                            d = Jsoup.connect(programRootUrl + "?page=" + currentPage++).get();
+                        } while(currentPage <= totalNumberOfPages);
                     }
                 }
             }
-            listOfEpisodes.forEach(System.out::println);
-            System.out.println("ACCUMULATED NUMBER OF EPISODES: " + totalNumberOfEpisodesFound);
-            System.out.println("SIZE OF EPISODES' LIST" + listOfEpisodes.size());
-        } catch(IOException ioe) {
-            System.out.println("IO error at " + listOfPrograms.get(index));
         }
+        listOfEpisodes.forEach(System.out::println);
+        System.out.println(String.format("%31s", "ACCUMULATED NUMBER OF EPISODES:") + totalNumberOfEpisodesFound);
+        System.out.println(String.format("%31s", "SIZE OF EPISODES' LIST ") + listOfEpisodes.size());
+        return listOfEpisodes;
     }
 
     /*
@@ -200,6 +170,17 @@ public class WebCrawler {
             });
         }
         listOfPrograms.forEach(System.out::println);
+        return listOfPrograms;
+    }
+
+    /*
+     *
+     */
+    private static List<String> getSelectedProgramsList() {
+        List<String> listOfPrograms = new ArrayList();
+        for (String[] program : selectedPrograms) {
+            listOfPrograms.add(program[1]);
+        }
         return listOfPrograms;
     }
 }
