1e8f1c1972d63ebc5fa4c46df44fb7dc5eb59aa3
nardos <32473919+nardost@users.noreply.github.com>
2020-03-13 11:37:15 -0500

    The number of programs limited to only a select few

--- princeton-web-crawler/src/main/java/edu/depaul/ntessema/csc575/WebCrawler.java
+++ princeton-web-crawler/src/main/java/edu/depaul/ntessema/csc575/WebCrawler.java
@@ -10,43 +10,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WebCrawler {
+
+    /*
+     * The number of available BBC programs is to great to deal with
+     * in this course project. So, I had to limit the programs to only
+     * a subset of the entire program list.
+     */
+    private static String[][] selectedPrograms = new String [][] {
+        { "The-Why-Factor", "https://www.bbc.co.uk/programmes/p00xtky9/episodes/player" },
+        { "The-Forum", "https://www.bbc.co.uk/programmes/p004kln9/episodes/player" },
+        { "Thinking-Allowed", "https://www.bbc.co.uk/programmes/b006qy05/episodes/player" },
+        { "In-Our-Time", "https://www.bbc.co.uk/programmes/b006qykl/episodes/player" },
+        { "Start-the-Week", "https://www.bbc.co.uk/programmes/b006r9xr/episodes/player" },
+        { "The-Fifth-Floor", "https://www.bbc.co.uk/programmes/p00mt9kd/episodes/player" },
+        { "Crossing-Continents", "https://www.bbc.co.uk/programmes/b006qt55/episodes/player" },
+        { "The-History-Hour", "https://www.bbc.co.uk/programmes/p016tmg1/episodes/player" },
+        { "Free-Thinking", "https://www.bbc.co.uk/programmes/b0144txn/episodes/player" },
+        { "The Documentary", "https://www.bbc.co.uk/programmes/p00fvhsf/episodes/player" },
+        { "Last-Word", "https://www.bbc.co.uk/programmes/b006qpmv/episodes/player" },
+        { "The-Essay", "https://www.bbc.co.uk/programmes/b006x3hl/episodes/player" },
+        { "Arts-and-Ideas", "https://www.bbc.co.uk/programmes/b0144txn/episodes/player" },
+        { "Heart-and-Soul", "https://www.bbc.co.uk/programmes/p002vsn4/episodes/player" },
+        { "Great-Lives", "https://www.bbc.co.uk/programmes/b006qxsb/episodes/player" },
+        { "Night-Waves", "https://www.bbc.co.uk/programmes/b006tp43/episodes/player" },
+        { "Meridian", "https://www.bbc.co.uk/programmes/p03m0hz8/episodes/guide" },
+        { "Discovery", "https://www.bbc.co.uk/programmes/p002w557/episodes/player" },
+        { "A-History-of-Ideas", "https://www.bbc.co.uk/programmes/b04bwydw/episodes/player" },
+        { "Outlook", "https://www.bbc.co.uk/programmes/p002vsxt/episodes/player" }
+    };
+
     public static void main(String[] args) {
-        final int MAX_NUMBEER_OF_EPISODES = 10;
-        /*
-         * List that holds all BBC Radio programs.
-         */
+
         List<String> listOfPrograms = new ArrayList<>();
+
+        for(String [] program : selectedPrograms) {
+            /*
+             * Limit the list even more...
+             */
+            if(program[0].equals("A-History-of-Ideas")) {
+                listOfPrograms.add(program[1]);
+            }
+        }
+
         /*
          * List that holds all episodes of all programs.
          */
         List<String> listOfEpisodes = new ArrayList<>();
         int index = 0;
         try {
-            final String urlPrefix = "https://www.bbc.co.uk/programmes/a-z/by/";
-            final String urlSuffix = "/player";
+
             final String cssClass = "div.programme__body h2.programme__titles a";
 
-            String url;
-
-            /*
-             * Only pages a-z are selected for crawling.
-             */
-            for(int i = 0x61; i <= 0x7a; i++) {
-                char ch = (char) (i);
-                url = urlPrefix + ch + urlSuffix;
-                Document document = Jsoup.connect(url).get();
-                List<Element> programmes =  document.select(cssClass);
-                programmes.forEach((element) -> {
-                    String programLink = element.attr("href") + "/episodes/guide";
-                    /*
-                     * The cssClass is the same for episode links
-                     * add N episodes for each program
-                     */
-                    listOfPrograms.add(programLink);
-                });
-            }
-            listOfPrograms.forEach(System.out::println);
-            index = 0;
             int totalNumberOfEpisodesFound = 0;
             /*
              * Iterate over the list of programs found.
@@ -96,7 +110,7 @@ public class WebCrawler {
                                 /*
                                  * Get the last pagination element
                                  */
-                                Element lastElement = pageElements.get(pageElements.size() - 1).getAllElements().last();
+                                Element lastElement = paginationElements.get(paginationElements.size() - 1).getAllElements().last();
                                 /*
                                  * The total number of pages is the number in the last
                                  * pagination element.
@@ -148,5 +162,44 @@ public class WebCrawler {
         } catch(IOException ioe) {
             System.out.println("IO error at " + listOfPrograms.get(index));
         }
+    }
+
+    /*
+     * The entire collection of BBC programs is too large to
+     * deal with in a mini-course project. So, I will not attempt
+     * to crawl the entire collection. However, if the program is
+     * to be scaled up to crawl the entire collection, this
+     * method (which, for now, crawls only the first pages of the
+     * a to z list) can be used.
+     */
+    private static List<String> findBBCPrograms() throws IOException {
+        final String urlPrefix = "https://www.bbc.co.uk/programmes/a-z/by/";
+        final String urlSuffix = "/player";
+        final String cssClass = "div.programme__body h2.programme__titles a";
+        /*
+         * List that holds all BBC Radio programs.
+         */
+        List<String> listOfPrograms = new ArrayList<>();
+        String url;
+
+        /*
+         * Only pages a-z are selected for crawling.
+         */
+        for(int i = 0x61; i <= 0x7a; i++) {
+            char ch = (char) (i);
+            url = urlPrefix + ch + urlSuffix;
+            Document document = Jsoup.connect(url).get();
+            List<Element> programmes =  document.select(cssClass);
+            programmes.forEach((element) -> {
+                String programLink = element.attr("href") + "/episodes/guide";
+                /*
+                 * The cssClass is the same for episode links
+                 * add N episodes for each program
+                 */
+                listOfPrograms.add(programLink);
+            });
+        }
+        listOfPrograms.forEach(System.out::println);
+        return listOfPrograms;
     }
 }
